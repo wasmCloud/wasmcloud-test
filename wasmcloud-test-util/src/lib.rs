c1b01491bba39cfe033a9ee62ba0c9baa584aaa1
Victor Adossi <vadossi@cosmonic.com>
2023-07-20 04:30:35 +0900

    refactor: remove smithy-bindgen dependency
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- wasmcloud-test-util/src/lib.rs
+++ wasmcloud-test-util/src/lib.rs
@@ -8,66 +8,8 @@ pub mod cli;
 pub use nkeys;
 pub use regex;
 
-pub mod testing {
-
-    smithy_bindgen::smithy_bindgen!("testing/testing.smithy", "org.wasmcloud.interface.testing");
-
-    // after sdk is split we won't have to duplicate this code
-    impl Default for TestOptions {
-        fn default() -> TestOptions {
-            TestOptions {
-                patterns: vec![".*".to_string()],
-                options: std::collections::HashMap::default(),
-            }
-        }
-    }
-
-    /// A NamedResult, generated inside a `run_selected!`` or `run_selected_spawn!`` macro,
-    /// contains a tuple of a test case name and its result. The implementation of From here
-    /// makes it easy to use the `into()` function to turn the NamedResult into a TestResult.
-    pub type NamedResult<'name, T> = (&'name str, RpcResult<T>);
-
-    // convert empty RpcResult into a testResult
-    impl<'name, T: Serialize> From<NamedResult<'name, T>> for TestResult {
-        fn from((name, res): NamedResult<'name, T>) -> TestResult {
-            let name = name.into();
-            match res {
-                Ok(res) => {
-                    // test passed. Serialize the data to json
-                    let data = match serde_json::to_vec(&res) {
-                        Ok(v) => serde_json::to_vec(&serde_json::json!({ "data": v }))
-                            .unwrap_or_default(),
-                        // if serialization of data fails, it doesn't change
-                        // the test result, but serialization errors should be logged.
-                        // Logging requires us to have logging set up, but since we might be running as an actor,
-                        // and we can't force the user to add a logging dependency and set up logging,
-                        // so there isn't much we can do here. Not even println!().
-                        Err(_) => b"".to_vec(),
-                    };
-                    TestResult {
-                        name,
-                        passed: true,
-                        snap_data: Some(data),
-                    }
-                }
-                Err(e) => {
-                    // test failed: generate an error message
-                    let data = serde_json::to_vec(&serde_json::json!(
-                        {
-                           "error": e.to_string(),
-                        }
-                    ))
-                    .ok();
-                    TestResult {
-                        name,
-                        passed: false,
-                        snap_data: data,
-                    }
-                }
-            }
-        }
-    }
-}
+// re-export testing interface
+pub use wasmcloud_interface_testing as testing;
 
 // these macros are supported on all build targets (wasm32 et. al.)
 
